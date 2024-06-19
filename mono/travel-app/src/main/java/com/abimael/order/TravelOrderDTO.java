14b5b757f4c28985a389ab18a9500d4fa9d0563e
Abimael Sergio <abimaelr.sergio@gmail.com>
2024-06-18 22:33:10 -0300

    Parte dois

---
+++ mono/travel-app/src/main/java/com/abimael/order/TravelOrderDTO.java
@@ -0,0 +1,50 @@
+package com.abimael.order;
+
+import com.abimael.flight.Flight;
+import com.abimael.hotel.Hotel;
+
+public class TravelOrderDTO {
+    private String fromAirport;
+    private String toAirport;
+    private Integer nights;
+
+    public TravelOrderDTO(){}
+
+    private TravelOrderDTO(String fromAirport, String toAirport, Integer nights) {
+        this.fromAirport = fromAirport;
+        this.toAirport = toAirport;
+        this.nights = nights;
+    }
+
+    public static TravelOrderDTO of(TravelOrder travelOrder, Flight flight, Hotel hotel) {
+        return new TravelOrderDTO(flight.fromAirport, flight.toAirport, hotel.nights);
+    }
+
+    private static TravelOrderDTO of(String fromAirport, String toAirport, Integer nights) {
+        return new TravelOrderDTO(fromAirport, toAirport, nights);
+    }
+
+    public String getFromAirport() {
+        return fromAirport;
+    }
+
+    public void setFromAirport(String fromAirport) {
+        this.fromAirport = fromAirport;
+    }
+
+    public String getToAirport() {
+        return toAirport;
+    }
+
+    public void setToAirport(String toAirport) {
+        this.toAirport = toAirport;
+    }
+
+    public Integer getNights() {
+        return nights;
+    }
+
+    public void setNights(Integer nights) {
+        this.nights = nights;
+    }
+}
