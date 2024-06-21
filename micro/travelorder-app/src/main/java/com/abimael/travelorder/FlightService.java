f35c9a61305f8aaf4cef71165d514a8927b97be3
Abimael Sergio <abimaelr.sergio@gmail.com>
2024-06-21 10:41:26 -0300

    Parte quatro

--- micro/travelorder-app/src/main/java/com/abimael/travelorder/FlightService.java
+++ micro/travelorder-app/src/main/java/com/abimael/travelorder/FlightService.java
@@ -1,5 +1,7 @@
 package com.abimael.travelorder;
 
+import java.time.temporal.ChronoUnit;
+
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
@@ -7,6 +9,9 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @RegisterRestClient(baseUri = "http://localhost:8081/flight")
@@ -20,10 +25,27 @@ public interface FlightService {
     @GET
     @Path("findByTravelOrderId")
     @Produces(MediaType.APPLICATION_JSON)
+    @Timeout(unit = ChronoUnit.SECONDS, value = 2)
+    @Fallback(fallbackMethod = "fallback")
+    @CircuitBreaker(
+            requestVolumeThreshold = 4, // A cada 4 amostragens (requests) faça uma verificação
+            failureRatio = 0.5, // qual a porcentagem de falha vou usar para assumir qeu existe um problema?
+            delay = 5000,// Aguardar e depois voltar a deixar conectar (ligar o disjuntor)
+            successThreshold = 2// quantas requisições com sucesso para eu assumir que o problema sumiu?
+    )
     Flight findByTravelOrderId(@QueryParam("travelOrderId") long travelOrderId);
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     void newFlight(Flight flight);
+
+    // O método de fallback tem que ter os mesmos parâmetros e tipo de retorno
+    default Flight fallback(long travelOrderId) {
+        Flight flight = new Flight();
+        flight.setFromAirport("");
+        flight.setToAirport("");
+        flight.setTravelOrderId(travelOrderId);
+        return flight;
+    }
 }
--- micro/travelorder-app/src/main/java/com/abimael/travelorder/HotelService.java
+++ micro/travelorder-app/src/main/java/com/abimael/travelorder/HotelService.java
@@ -1,5 +1,7 @@
 package com.abimael.travelorder;
 
+import java.time.temporal.ChronoUnit;
+
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
@@ -7,6 +9,9 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @RegisterRestClient(baseUri = "http://localhost:8082/hotel")
@@ -15,15 +20,31 @@ public interface HotelService {
     @GET
     @Path("findById")
     @Produces(MediaType.APPLICATION_JSON)
-    public Hotel findById(@QueryParam("id") long id);
+    Hotel findById(@QueryParam("id") long id);
 
     @GET
     @Path("findByTravelOrderId")
     @Produces(MediaType.APPLICATION_JSON)
-    public Hotel findByTravelOrderId(@QueryParam("travelOrderId") long travelOrderId);
+    @Timeout(unit = ChronoUnit.SECONDS, value = 2)
+    @Fallback(fallbackMethod = "fallback")
+    @CircuitBreaker(
+            requestVolumeThreshold = 4, // A cada 4 amostragens (requests) faça uma verificação
+            failureRatio = 0.5, // qual a porcentagem de falha vou usar para assumir qeu existe um problema?
+            delay = 5000,// Aguardar e depois voltar a deixar conectar (ligar o disjuntor)
+            successThreshold = 2// quantas requisições com sucesso para eu assumir que o problema sumiu?
+    )
+    Hotel findByTravelOrderId(@QueryParam("travelOrderId") long travelOrderId);
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Hotel newHotel(Hotel hotel);
+    Hotel newHotel(Hotel hotel);
+
+    // O método de fallback tem que ter os mesmos parâmetros e tipo de retorno
+    default Hotel fallback(long travelOrderId) {
+        Hotel hotel = new Hotel();
+        hotel.setNights(-1);
+        hotel.setTravelOrderId(travelOrderId);
+        return hotel;
+    }
 }
